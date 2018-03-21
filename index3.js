1d642251afcebdbb84138989e71a274fdb23dd82
Javier <jcoucelanga@gmail.com>
2018-03-21 21:28:12 +0100

    añadido componente header

---
+++ index3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                THIS IS A HEADER
+            </div>
+        );
+    }
+}
+
+export default Header;
